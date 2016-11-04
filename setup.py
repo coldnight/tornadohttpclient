6c9a9d3c5fa554e7a28774e5b7ff3330ac00ff4c
wanghui <wh_linux@126.com>
2016-11-04 12:24:53 +0800

    Bump version & fix setup.py flake8

--- setup.py
+++ setup.py
@@ -5,19 +5,23 @@ from setuptools import setup
 
 requires = ['tornado', 'pycurl']
 
-setup(name='tornadohttpclient',
-      version= '1.0.1',
-      description='Asynchronous http client.',
-      long_description="TornadoHTTPClient 对tornado.curl_httpclient.CurlAsyncHTTPClient的封装, 支持cookie",
-      author='cold',
-      author_email='wh_linux@126.com',
-      url='http://www.linuxzen.com',
-      py_modules=['tornadohttpclient'],
-      scripts=[],
-      install_requires = requires,
-      license='Apache 2.0',
-      platforms = 'any',
-      classifiers=['Development Status :: 3 - Alpha',
+setup(
+    name='tornadohttpclient',
+    version='1.1.0',
+    description='Asynchronous http client.',
+    long_description=("TornadoHTTPClient 对"
+                      "tornado.curl_httpclient.CurlAsyncHTTPClient的封装, "
+                      "支持cookie"),
+    author='cold',
+    author_email='wh_linux@126.com',
+    url='http://www.linuxzen.com',
+    py_modules=['tornadohttpclient'],
+    scripts=[],
+    install_requires=requires,
+    license='Apache 2.0',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
         "Intended Audience :: Developers",
         'License :: OSI Approved :: Apache Software License',
         'Topic :: Internet :: WWW/HTTP',
